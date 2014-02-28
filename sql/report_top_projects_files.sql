cd148edf27bdd3171958fa758d829a15663a9183
eric-wu <eric.wu@sagebase.org>
2014-02-27 22:43:30 -0800

    Renaming and other refactoring.

---
+++ sql/report_top_projects_files.sql
@@ -0,0 +1,91 @@
+-- Top public projects
+-- 8 minutes
+SELECT
+    PN.ID,
+    PN.NAME,
+    COUNT(DISTINCT AR.SESSION) AS SESSION_COUNT
+FROM
+    AUDIT_PUBLIC_NODES PN
+INNER JOIN
+    AUDIT_ACCESS_RECORDS AR
+ON
+    PN.ID = AR.ENTITY_ID AND PN.TYPE = 2 -- project
+GROUP BY
+    PN.ID,
+    PN.NAME
+ORDER BY
+    COUNT(DISTINCT AR.SESSION) DESC;
+
+-- Top public projects accessed by non-Sage users
+-- 115 minutes
+SELECT
+    PN.ID,
+    PN.NAME,
+    COUNT(DISTINCT AR.SESSION) AS SESSION_COUNT
+FROM
+    AUDIT_PUBLIC_NODES PN
+INNER JOIN
+    AUDIT_ACCESS_RECORDS AR
+ON
+    PN.ID = AR.ENTITY_ID AND PN.TYPE = 2 -- project
+INNER JOIN
+    AUDIT_NON_SAGE_USERS NSU
+ON
+    AR.USER_ID = NSU.ID
+GROUP BY
+    PN.ID,
+    PN.NAME
+ORDER BY
+    SESSION_COUNT DESC;
+
+
+-- Top public file downloads
+-- 12 minutes
+SELECT
+    PN.ID,
+    PN.NAME,
+    COUNT(DISTINCT AR.SESSION) AS SESSION_COUNT
+FROM
+    AUDIT_PUBLIC_NODES PN
+INNER JOIN
+    AUDIT_ACCESS_RECORDS AR
+ON
+    PN.ID = AR.ENTITY_ID AND PN.TYPE = 16 -- file
+WHERE
+    AR.METHOD = 'GET' AND
+    -- File download
+    -- /repo/v1/entity/{id}/file
+    -- /repo/v1/entity/{id}/version/{ver}/file
+    AR.URI REGEXP '^/repo/v1/entity/syn[0-9]+/.*/file$'
+GROUP BY
+    PN.ID,
+    PN.NAME
+ORDER BY
+    SESSION_COUNT DESC;
+
+-- Top public file downloads by non-Sage users
+SELECT
+    PN.ID,
+    PN.NAME,
+    COUNT(DISTINCT AR.SESSION) AS SESSION_COUNT
+FROM
+    AUDIT_PUBLIC_NODES PN
+INNER JOIN
+    AUDIT_ACCESS_RECORDS AR
+ON
+    PN.ID = AR.ENTITY_ID AND PN.TYPE = 16 -- file
+INNER JOIN
+    AUDIT_NON_SAGE_USERS NSU
+ON
+    AR.USER_ID = NSU.ID
+WHERE
+    AR.METHOD = 'GET' AND
+    -- File download
+    -- /repo/v1/entity/{id}/file
+    -- /repo/v1/entity/{id}/version/{ver}/file
+    AR.URI REGEXP '^/repo/v1/entity/syn[0-9]+/.*/file$'
+GROUP BY
+    PN.ID,
+    PN.NAME
+ORDER BY
+    SESSION_COUNT DESC;
---
+++ sql/table_access_records.sql
@@ -0,0 +1,119 @@
+-- The object-id column of the access records does not always capture
+-- the entity IDs in the responses. These are entity IDs logged in
+-- the request URIs. Use this table as a supplement table for entity IDs.
+CREATE TABLE AUDIT_SESSION_ENTITY (
+    SESSION CHAR(36) NOT NULL,
+    ENTITY_ID BIGINT NOT NULL
+);
+
+-- 3,167,850 rows, 1.5 minutes
+LOAD DATA LOCAL INFILE
+    '/Users/ewu/Documents/logs/audit-2014-02/results/session-synid.csv'
+INTO TABLE
+    AUDIT_SESSION_ENTITY
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n' (
+    SESSION,
+    ENTITY_ID
+);
+
+-- 1 minute
+ALTER TABLE AUDIT_SESSION_ENTITY ADD INDEX USING HASH (SESSION);
+-- 0.5 minute
+ALTER TABLE AUDIT_SESSION_ENTITY ADD INDEX USING HASH (ENTITY_ID);
+
+
+-- Selected columns of the access records
+CREATE TABLE AUDIT_ACCESS_RECORDS (
+    OBJECT_ID_IMPORT VARCHAR(12),
+    ENTITY_ID BIGINT,
+    LATENCY BIGINT,
+    TIMESTAMP_IMPORT BIGINT,    -- Import as long integers first
+    TIMESTAMP DATETIME,         -- Then convert to date time
+    USER_AGENT VARCHAR(256),
+    SESSION CHAR(36),
+    URI VARCHAR(256),
+    USER_ID_IMPORT VARCHAR(12), -- Import as strings (as some are empty strings)
+    USER_ID INT,                -- Then convert to integers
+    METHOD VARCHAR(12),
+    STACK INT,
+    SUCCESS VARCHAR(5)
+);
+
+-- Imports merged access-record files of selected columns
+-- 21,731,753 rows, 45 minutes on a medium instance
+LOAD DATA LOCAL INFILE
+    '/Users/ewu/Documents/logs/audit-2014-02/results/access-record-lean.csv' 
+INTO TABLE
+    AUDIT_ACCESS_RECORDS
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n' (
+    OBJECT_ID_IMPORT,
+    LATENCY,
+    TIMESTAMP_IMPORT,
+    USER_AGENT,
+    SESSION,
+    URI,
+    USER_ID_IMPORT,
+    METHOD,
+    STACK,
+    SUCCESS
+);
+
+-- Converts imported columns to proper types and values
+-- 25 minutes
+UPDATE
+    AUDIT_ACCESS_RECORDS
+SET
+    TIMESTAMP = FROM_UNIXTIME(TIMESTAMP_IMPORT / 1000);
+
+-- 25 minutes
+UPDATE
+    AUDIT_ACCESS_RECORDS
+SET
+    USER_ID = CAST(USER_ID_IMPORT AS UNSIGNED)
+WHERE
+    USER_ID_IMPORT IS NOT NULL AND
+    USER_ID_IMPORT <> '';
+
+-- 5.5 minutes
+UPDATE
+    AUDIT_ACCESS_RECORDS
+SET
+    ENTITY_ID = CAST(SUBSTRING(OBJECT_ID_IMPORT, 4) AS UNSIGNED)
+WHERE
+    OBJECT_ID_IMPORT LIKE 'syn%' OR
+    OBJECT_ID_IMPORT LIKE 'SYN%';
+
+-- Adds indices
+ALTER TABLE AUDIT_ACCESS_RECORDS ADD INDEX USING BTREE (TIMESTAMP); -- 4.5 minutes
+ALTER TABLE AUDIT_ACCESS_RECORDS ADD INDEX USING HASH (USER_ID);    -- 4.5 minutes
+ALTER TABLE AUDIT_ACCESS_RECORDS ADD INDEX USING HASH (ENTITY_ID);  -- 4 minutes
+ALTER TABLE AUDIT_ACCESS_RECORDS ADD INDEX USING HASH (SESSION);    -- 9 minutes
+
+
+-- Merges in the entity IDs parsed from the URIs
+-- 37 minutes
+UPDATE
+    AUDIT_ACCESS_RECORDS AR, AUDIT_SESSION_ENTITY SE
+SET
+    AR.ENTITY_ID = SE.ENTITY_ID
+WHERE
+    AR.SESSION = SE.SESSION AND
+    AR.ENTITY_ID IS NULL; -- Set the entity ID from URI only if the entity ID is missing
+
+
+-- Unit tests
+SELECT CONCAT(CASE WHEN
+    (SELECT COUNT(DISTINCT ENTITY_ID) FROM AUDIT_ACCESS_RECORDS WHERE ENTITY_ID IS NOT NULL) > 50000
+    THEN 'PASSED' ELSE 'FAILED' END,
+    ' -- There should be at least 50,000 entities with activitieis during the quarter.');
+
+SELECT CONCAT(CASE WHEN
+    (SELECT COUNT(DISTINCT USER_ID) FROM AUDIT_ACCESS_RECORDS WHERE USER_ID IS NOT NULL) > 1000
+    THEN 'PASSED' ELSE 'FAILED' END,
+    ' -- There should be at least 1000 users with activities during the quarter.');
+
+SELECT * FROM AUDIT_ACCESS_RECORDS LIMIT 300;
