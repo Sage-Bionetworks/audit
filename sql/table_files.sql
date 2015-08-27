910840505fdb3780525ee05c8116d7e33a26a31c
Kimyen Truong <kimyentruong@Kimyen-MacBook.local>
2015-08-26 22:29:27 -0700

    change based on Synapse table schema changes

--- sql/table_files.sql
+++ sql/table_files.sql
@@ -1,40 +1,17 @@
+DROP TABLE AUDIT_FILES;
 -- File entities and their containing project
 CREATE TABLE AUDIT_FILES AS
 SELECT DISTINCT
-    NODE.ID AS ID,
+	NODE.ID AS ID,
     NODE.NAME AS NAME,
     FROM_UNIXTIME(NODE.CREATED_ON / 1000) AS CREATED_ON,
     NODE.CREATED_BY AS CREATED_BY,
-    -- Back track at most 16 levels to find the containing project
-    -- The first non-null parent is the containing project
-    -- Or there can be a special case where the file has no containing project (24 such files) and
-    -- the parent project will back track all the way to the root (ID 4489)
-    CASE
-        WHEN P16.ID IS NOT NULL THEN -1 -- Parent recursion overflow
-        ELSE NULLIF(COALESCE(P15.ID, P14.ID, P13.ID, P12.ID, P11.ID, P10.ID, P9.ID, P8.ID,
-            P7.ID, P6.ID, P5.ID, P4.ID, P3.ID, P2.ID, P1.ID), 4489)
-    END AS PROJECT_ID
-FROM
-    JDONODE NODE
-    LEFT JOIN JDONODE P1 ON NODE.PARENT_ID = P1.ID
-    LEFT JOIN JDONODE P2 ON (P1.PARENT_ID = P2.ID AND P1.NODE_TYPE != 'project') -- Type 2 is project
-    LEFT JOIN JDONODE P3 ON (P2.PARENT_ID = P3.ID AND P2.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P4 ON (P3.PARENT_ID = P4.ID AND P3.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P5 ON (P4.PARENT_ID = P5.ID AND P4.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P6 ON (P5.PARENT_ID = P6.ID AND P5.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P7 ON (P6.PARENT_ID = P7.ID AND P6.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P8 ON (P7.PARENT_ID = P8.ID AND P7.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P9 ON (P8.PARENT_ID = P9.ID AND P8.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P10 ON (P9.PARENT_ID = P10.ID AND P9.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P11 ON (P10.PARENT_ID = P11.ID AND P10.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P12 ON (P11.PARENT_ID = P12.ID AND P11.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P13 ON (P12.PARENT_ID = P13.ID AND P12.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P14 ON (P13.PARENT_ID = P14.ID AND P13.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P15 ON (P14.PARENT_ID = P15.ID AND P14.NODE_TYPE != 'project')
-    LEFT JOIN JDONODE P16 ON (P15.PARENT_ID = P16.ID AND P15.NODE_TYPE != 'project')
+    NODE.PROJECT_ID AS PROJECT_ID
+FROM 
+	JDONODE NODE
 WHERE
-    NODE.NODE_TYPE = 'file' AND        -- Type 16 is file
-    NODE.BENEFACTOR_ID <> 1681355; -- Not in trash can
+	NODE.NODE_TYPE = 'file' AND 
+    NODE.BENEFACTOR_ID <> 1681355; -- NOT IN TRASH CAN
 
 -- Adds indices
 ALTER TABLE AUDIT_FILES ADD INDEX USING HASH (ID);
@@ -64,7 +41,7 @@ SELECT CONCAT(CASE WHEN
     ' -- 0 files in the project of getting started with the R client');
 
 SELECT CONCAT(CASE WHEN
-    (SELECT COUNT(ID) FROM AUDIT_FILES WHERE ID = 1670933) = 0
+    (SELECT COUNT(ID) FROM AUDIT_FILES WHERE ID = 'file'70933) = 0
     THEN 'PASSED' ELSE 'FAILED' END,
     ' -- Test the file Controlled Use Example. This is not a public file.');
 
